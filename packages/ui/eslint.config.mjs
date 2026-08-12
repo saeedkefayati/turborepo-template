96cd92d6a5ef189c075f9960608de3fdc8c56fff
Saeed Kefayati <s.kefayati2012@gmail.com>
2026-08-13 02:55:51 +0330

    feat(docs): migrate documentation site to Docusaurus

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,5 +1,5 @@
 import storybook from "eslint-plugin-storybook";
 
-import { config } from "@repo/eslint-config/react-internal";
+import { config as reactConfig } from "@repo/eslint-config/react-internal";
 
-export default [...config, ...storybook.configs["flat/recommended"]];
+export default [...reactConfig, ...storybook.configs["flat/recommended"]];
